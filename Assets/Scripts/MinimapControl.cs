7c94db3f800f91f46f165235f88e1e85decc66db
Aleks Molchanov <aleksandr.molcanov@gmail.com>
2017-08-02 20:28:41 +0200

    no message

--- Assets/Scripts/MinimapControl.cs
+++ Assets/Scripts/MinimapControl.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+	
+	//kvar att skapa: public variabler för camSize minimum och maximum
 
 public class MinimapControl : MonoBehaviour {
-
+	//deklarera variabler: objektet, dess component, och variabeln camSize:
 	public GameObject minimapCamera;
 	public Camera camera;
 	public float camSize;
 
 	// Use this for initialization
 	void Awake () {
+		//här hugger vi objektets komponent Camera och sätter den i variabeln camera
 		camera = minimapCamera.GetComponent<Camera>();
 		//camSize = minimapCamera.GetComponent<Camera>.orthographicSize;
 	}
