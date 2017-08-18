5195d5e63677cdde5130bcb28f65d9a59cd70ac1
Aleks Molchanov <aleksandr.molcanov@gmail.com>
2017-08-18 23:50:27 +0200

    Shaderpill i separat scen. Nothing to look at.

---
+++ Assets/Scripts/ShaderScripts/CustomImageEffects.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CustomImageEffects : MonoBehaviour {
+
+	public Material EffectMaterial;
+
+	// Use this for initialization
+	void OnRenderImage (RenderTexture src, RenderTexture dst) {
+		if(EffectMaterial != null){
+			Graphics.Blit(src, dst, EffectMaterial);
+		}
+	}
+
+}
