af64f9d3d6e2e7bdafc7ed450a3c5c16634da9e5
matol004 <mattias.olsson.1@hotmail.com>
2018-01-17 21:20:37 +0100

    Added homing rocket as the new default. Lock with MouseButton2.

--- Assets/Scripts/Weapons/Rocket.cs
+++ Assets/Scripts/Weapons/Rocket.cs
@@ -2,38 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
-
-    //    private float birthTime = 0f;
-    public int Damage = 10;
-    public float bulletSpeed = 40f;
-    public Rigidbody rb;
-
-    void Awake()
-    {
-//        birthTime = Time.time;
-    }
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        Destroy(gameObject, 30f);
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        GameObject hitGo = col.gameObject;
-        IHealth hitGoHealth = hitGo.GetComponent<IHealth>();
-        if(hitGoHealth != null)
-        {
-            hitGoHealth.DoDamage(Damage);
-        }
-        Destroy(gameObject, 0.001f);
-    }
-
+public class Rocket : WeaponBase {
     void FixedUpdate()
     {
-        rb.MovePosition(transform.localPosition + transform.forward * bulletSpeed * Time.deltaTime);
+        MoveForward();
     }
-    
 }
