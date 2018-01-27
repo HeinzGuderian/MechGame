6d90d661d9db4b555bc6798ef30ca4a0a2d07f03
ALEKSANDR MOLCANOV <aleksandr.molcanov@gmail.com>
2018-01-27 22:15:34 +0100

    textboxar som flyter ovanpå 3d-knappar är nu möjliga.

--- Assets/Scripts/MechGUI/KeyTextRender.cs
+++ Assets/Scripts/MechGUI/KeyTextRender.cs
@@ -1,31 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyTextRender : MonoBehaviour {
-	public Transform target;
-	public string renderKey;
-	Camera cam;
-	// Use this for initialization
-	void Start () {
-		//renderKey = "KEY";
-		//cam = GetComponent<Camera>();
-		target = GetComponent<Transform>();
-	}
+
+	public Text buttonLabel;
+	public Camera cam;
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 labelPos = cam.WorldToScreenPoint(this.transform.position);
+		buttonLabel.transform.position = labelPos;
 	}
-	void OnGUI(){
-        GUI.enabled=true;
-		cam=Camera.current;
-        //Vector3 screenPos = cam.WorldToViewportPoint(target.position);
-        Vector3 pos=cam.WorldToViewportPoint(target.position);
-        
-        Debug.Log("TARGET POSITION:" + target.position + "Vector3 pos:" + pos);
-        //Rita ut en GUI Label med text en specifik position:
-        GUI.Label(new Rect(pos.x,pos.y,150,130),renderKey);
-        //GUI.Label(new Rect(0,0,150,130),renderKey);
-    }
+	
 }
