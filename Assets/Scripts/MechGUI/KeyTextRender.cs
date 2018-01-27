8198f1c2f6cf0d1d813f769900723adb9da08466
ALEKSANDR MOLCANOV <aleksandr.molcanov@gmail.com>
2018-01-27 22:56:27 +0100

    no message

--- Assets/Scripts/MechGUI/KeyTextRender.cs
+++ Assets/Scripts/MechGUI/KeyTextRender.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class KeyTextRender : MonoBehaviour {
-
+	public string label;
 	public Text buttonLabel;
 	public Camera cam;
-
+	void Start(){
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 labelPos = cam.WorldToScreenPoint(this.transform.position);
 		buttonLabel.transform.position = labelPos;
+		
 	}
-	
+	//void OnGUI(){
+		//Vector3 labelPos = cam.WorldToScreenPoint(this.transform.position);
+		//GUI.Label(new Rect(labelPos.x, Screen.height-labelPos.y, 150, 130),label); 
+	//}
 }
